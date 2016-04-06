ebeaab75ee45ac8a3827aa73fb0bc5bb22dc17ee
cocci852 <cnguye21@gmail.com>
2016-04-05 20:40:07 -0700

    update 4-5-16

---
+++ app/src/main/java/com/houseonacliff/sourdoughstartersimulation/LocationDialog.java
@@ -0,0 +1,28 @@
+package com.houseonacliff.sourdoughstartersimulation;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+
+/**
+ * Created by cocci852 on 4/5/2016.
+ */
+public class LocationDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(),R.style.AppTheme_Dialog));
+        builder.setTitle(R.string.pick_location)
+                .setItems(R.array.location_array, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int location_id) {
+                        //0=counter, 1=pantry, 2=fridge
+
+
+                    }
+                });
+        return builder.create();
+    }
+}
--- app/src/main/java/com/houseonacliff/sourdoughstartersimulation/MainActivity.java
+++ app/src/main/java/com/houseonacliff/sourdoughstartersimulation/MainActivity.java
@@ -1,13 +1,107 @@
 package com.houseonacliff.sourdoughstartersimulation;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    //States
+    boolean isJarFull = false;
+    boolean isLidOn = false;
+
+    //Buttons
+    Button lidButton;
+    Button feedButton;
+
+    //Jar Animation
+    ObjectAnimator moveJarUpAnimation;
+    ObjectAnimator moveJarDownAnimation;
+    ObjectAnimator moveLidUpAnimation;
+    ObjectAnimator moveLidDownAnimation;
+    PropertyValuesHolder translationUp;
+    PropertyValuesHolder translationDown;
+    PropertyValuesHolder fadeIn;
+    PropertyValuesHolder fadeOut;
+
+
+    ImageView lidObject;
+    ImageView jarObject;
+
+    //constants based on dimens
+    float lidTranslationOn = 0f;
+    float lidTranslationOff = -400f;
+
+    LocationDialog locationMenu = new LocationDialog();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
+
+        lidButton = (Button) findViewById(R.id.lid_button);
+        feedButton = (Button) findViewById(R.id.feed_button);
+
+        lidObject = (ImageView) findViewById(R.id.lid_imageview);
+        jarObject = (ImageView) findViewById(R.id.jar_imageview);
+
+
+    }
+
+    public void onClick(int location_id) {
+
+    }
+
+
+    public void enableButtons () {
+        lidButton = (Button) findViewById(R.id.lid_button);
+        feedButton = (Button) findViewById(R.id.feed_button);
+        lidButton.setEnabled(true);
+        feedButton.setEnabled(true);
+    }
+
+    public void disableButtons () {
+        lidButton = (Button) findViewById(R.id.lid_button);
+        feedButton = (Button) findViewById(R.id.feed_button);
+        lidButton.setEnabled(false);
+        feedButton.setEnabled(false);
+    }
+
+    //Click LID/DELID button
+    public void changeLidState(View v) {
+        if(isLidOn) { //remove lid
+            isLidOn = false;
+            lidObject.animate().translationY(lidTranslationOff).alpha(0f).setDuration(500);
+            lidButton.setText(R.string.lid_button_add_lid);
+        }
+        else {//put on lid
+            isLidOn = true;
+            lidObject.animate().translationY(lidTranslationOn).alpha(1f).setDuration(500);
+            lidButton.setText(R.string.lid_button_remove_lid);
+        }
+
+
+    }
+
+    //Click to divide starter by half and add new flour + water
+    public void feedJar(View v) {
+
+    }
+
+    //Click to change location of Jar
+    public void changeLocation(View v) {
+        locationMenu.show(getFragmentManager(), "location");
+    }
+
+    //First feed
+    public void  firstFeed() {
+
     }
 }
